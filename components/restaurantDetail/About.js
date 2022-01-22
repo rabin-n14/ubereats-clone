1e4a35c3e14e4c8beffa6cc5d91ee4b2d8b4e3eb
rabin-n14 <74393889+rabin-n14@users.noreply.github.com>
2022-01-22 14:01:10 +1100

    Detail Screen img, title and des frontend

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -1,16 +1,46 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 const image = "https://images.thebrag.com/tb/uploads/2020/05/Hungry-Jacks.jpg";
 const title = "Bake and Shake";
-const description = "English · Comfort Food · $$ · 🎟 · 4 ⭐ (2913+)";
+const description = "English · Fast Food · $$ · 🎟 · 4 ⭐ (2913+)";
 
 export default function About() {
   return (
     <View>
-      {/* <RestaurantImage/>
-      <RestaurantTitle/>
-      <RestaurantDescription/> */}
+      <RestaurantImage image={image} />
+      <RestaurantTitle title={title} />
+      <RestaurantDescription description={description} />
     </View>
   );
 }
+
+const RestaurantImage = (props) => (
+  <Image source={{ uri: props.image }} style={{ width: "100%", height: 200 }} />
+);
+
+const RestaurantTitle = (props) => (
+  <Text
+    style={{
+      fontSize: 29,
+      fontWeight: "600",
+      marginTop: 10,
+      marginHorizontal: 15,
+    }}
+  >
+    {props.title}
+  </Text>
+);
+
+const RestaurantDescription = (props) => (
+  <Text
+    style={{
+      marginTop: 10,
+      marginHorizontal: 15,
+      fontWeight: "400",
+      fontSize: 15.5,
+    }}
+  >
+    {props.description}
+  </Text>
+);
