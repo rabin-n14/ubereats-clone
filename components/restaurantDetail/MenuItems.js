2773a04a52248e8ace60da17736ef65e3b07fc06
rabin-n14 <74393889+rabin-n14@users.noreply.github.com>
2022-01-29 02:04:36 +1100

    View Cart with Prices

--- components/restaurantDetail/MenuItems.js
+++ components/restaurantDetail/MenuItems.js
@@ -3,7 +3,7 @@ import { ScrollView } from "react-native";
 import { View, Text, StyleSheet, Image } from "react-native";
 import { Divider } from "react-native-elements/dist/divider/Divider";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const foods = [
   {
@@ -64,6 +64,13 @@ export default function MenuItems({ restaurantName }) {
       },
     });
 
+  const cartItems = useSelector(
+    (state) => state.cartReducer.selectedItems.items
+  );
+
+  const isFoodInCart = (food, cartItems) =>
+    Boolean(cartItems.find((item) => item.title === food.title));
+
   return (
     <>
       <View>
@@ -74,6 +81,7 @@ export default function MenuItems({ restaurantName }) {
                 <BouncyCheckbox
                   iconStyle={{ borderColor: "lightgray", borderRadius: 0 }}
                   fillColor="green"
+                  isChecked={isFoodInCart(food, cartItems)}
                   onPress={(checkboxValue) => selectItem(food, checkboxValue)}
                 />
                 <FoodInfo food={food} />
