fb23226de714bdd0fe0886e011b58a3e1d1f6bcd
rabin-n14 <74393889+rabin-n14@users.noreply.github.com>
2022-01-30 17:16:05 +1100

    Order Completed page from firebase

--- screens/OrderCompleted.js
+++ screens/OrderCompleted.js
@@ -1,9 +1,23 @@
-import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import { useSelector } from "react-redux";
 import LottieView from "lottie-react-native";
+import firebase from "../firebase";
+import MenuItems from "../components/restaurantDetail/MenuItems";
 
 export default function OrderCompleted() {
+  const [lastOrder, setLastOrder] = useState({
+    items: [
+      {
+        title: "Lasagna",
+        description: "With butter lettuce, tomato and sauce bechamel",
+        price: "$13.50",
+        image:
+          "https://www.modernhoney.com/wp-content/uploads/2019/08/Classic-Lasagna-14-scaled.jpg",
+      },
+    ],
+  });
+
   const { items, restaurantName } = useSelector(
     (state) => state.cartReducer.selectedItems
   );
@@ -16,24 +30,44 @@ export default function OrderCompleted() {
     currency: "USD",
   });
 
+  useEffect(() => {
+    const db = firebase.firestore();
+    const unsubscribe = db
+      .collection("orders")
+      .orderBy("createdAt", "desc")
+      .limit(1)
+      .onSnapshot((snapshot) => {
+        snapshot.docs.map((doc) => {
+          setLastOrder(doc.data());
+        });
+      });
+
+    return () => unsubscribe;
+  }, []);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-      <LottieView
-        style={{ height: 100, alignSelf: "center", marginBottom: 30 }}
-        source={require("../assets/animations/check-mark.json")}
-        autoPlay
-        speed={0.5}
-        loop={false}
-      />
-      <Text>
-        Your order at {restaurantName} has been placed for {totalUSD}
-      </Text>
-      <LottieView
-        style={{ height: 200, alignSelf: "center" }}
-        source={require("../assets/animations/cooking.json")}
-        autoPlay
-        speed={0.5}
-      />
+      <View style={{ margin: 15, alignItems: "center", height: "100%" }}>
+        <LottieView
+          style={{ height: 100, alignSelf: "center", marginBottom: 30 }}
+          source={require("../assets/animations/check-mark.json")}
+          autoPlay
+          speed={0.5}
+          loop={false}
+        />
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+          Your order at {restaurantName} has been placed for {totalUSD}
+        </Text>
+        <ScrollView>
+          <MenuItems foods={lastOrder.items} hideCheckbox={true} />
+        </ScrollView>
+        <LottieView
+          style={{ height: 200, alignSelf: "center" }}
+          source={require("../assets/animations/cooking.json")}
+          autoPlay
+          speed={0.5}
+        />
+      </View>
     </SafeAreaView>
   );
 }
